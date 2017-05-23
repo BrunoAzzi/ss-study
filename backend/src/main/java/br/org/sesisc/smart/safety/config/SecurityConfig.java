48095a5d27a1f8ff156f23557ba6af48b3aef22d
Fernando Pileggi <fernando@plyom.com.br>
2017-05-23 17:48:00 -0300

    simplify authorization

---
+++ backend/src/main/java/br/org/sesisc/smart/safety/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package br.org.sesisc.smart.safety.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/sessions").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .anonymous().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint());
+    }
+
+    @Bean
+    public AuthenticationProvider tokenAuthenticationProvider() {
+//        return new TokenAuthenticationProvider(tokenService());
+        return null;
+    }
+
+    @Bean
+    public AuthenticationEntryPoint unauthorizedEntryPoint() {
+        return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+}
---
+++ backend/src/main/java/br/org/sesisc/smart/safety/security/filters/AuthenticationFilter.java
@@ -0,0 +1,81 @@
+package br.org.sesisc.smart.safety.security.filters;
+
+import com.google.common.base.Strings;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.encoding.MessageDigestPasswordEncoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.google.common.base.Optional;
+import org.springframework.web.util.UrlPathHelper;
+
+public class AuthenticationFilter extends GenericFilterBean {
+
+    public static final String TOKEN_SESSION_KEY = "token";
+    private AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        Optional<String> token = Optional.fromNullable(httpRequest.getHeader("X-Authorization"));
+
+        String resourcePath = new UrlPathHelper().getPathWithinApplication(httpRequest);
+
+        try {
+            if(token.isPresent()) {
+                processTokenAuthentication(token);
+            }
+
+            addSessionContextToLogging();
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void addSessionContextToLogging() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String tokenValue = "EMPTY";
+        if (authentication != null && !Strings.isNullOrEmpty(authentication.getDetails().toString())) {
+            MessageDigestPasswordEncoder encoder = new MessageDigestPasswordEncoder("SHA-1");
+            tokenValue = encoder.encodePassword(authentication.getDetails().toString(), "not_so_random_salt");
+        }
+//        MDC.put(TOKEN_SESSION_KEY, tokenValue);
+    }
+
+    private void processTokenAuthentication(Optional<String> token) {
+        Authentication resultOfAuthentication = tryToAuthenticateWithToken(token);
+        SecurityContextHolder.getContext().setAuthentication(resultOfAuthentication);
+    }
+
+    private Authentication tryToAuthenticateWithToken(Optional<String> token) {
+        PreAuthenticatedAuthenticationToken requestAuthentication = new PreAuthenticatedAuthenticationToken(token, null);
+        return tryToAuthenticate(requestAuthentication);
+    }
+
+    private Authentication tryToAuthenticate(Authentication requestAuthentication) {
+        Authentication responseAuthentication = authenticationManager.authenticate(requestAuthentication);
+        if (responseAuthentication == null || !responseAuthentication.isAuthenticated()) {
+            throw new InternalAuthenticationServiceException("Unable to authenticate Domain User for provided credentials");
+        }
+        logger.debug("User successfully authenticated");
+        return responseAuthentication;
+    }
+
+}
