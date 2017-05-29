2f2374b459102c26c295e1d922bffa712a82ec83
Thamires Garcia <thamires.luz@outlook.com>
2017-05-29 11:24:33 -0300

    Merge no authentication

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -5,14 +5,15 @@ import { SessionsService } from '../services/sessions.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router, private sessionsService: SessionsService) { }
+  constructor(private router: Router, private sessionsService: SessionsService) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (this.sessionsService.isLoggedIn()) {
-            return true;
-        }
-
-        this.router.navigate(['/login']);
-        return false;
-    }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    // if (this.sessionsService.isLoggedIn()) {
+    //     return true;
+    // }
+    //
+    // this.router.navigate(['/login']);
+    // return false;
+    return true;
+  }
 }
