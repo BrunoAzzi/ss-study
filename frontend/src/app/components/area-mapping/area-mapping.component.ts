4b58c3d7251423775526835cf85bb38a474a08f5
Fernando Pileggi <fernando@plyom.com.br>
2017-06-06 13:46:49 -0300

    save mark by floor

--- frontend/src/app/components/area-mapping/area-mapping.component.ts
+++ frontend/src/app/components/area-mapping/area-mapping.component.ts
@@ -1,5 +1,7 @@
+import { Coordinate } from './../../models/coordinate.model';
 import { Floor } from './../../models/floor.model';
 import { Component } from '@angular/core';
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 @Component({
     selector: 'area-mapping',
@@ -9,21 +11,25 @@ import { Component } from '@angular/core';
 
 export class AreaMappingComponent {
 
+    barLevelStartIndex: any = new BehaviorSubject("");
+
     private map: any;
     private mapLayer: L.LayerGroup;
     private imageMap: any;
 
     private currentMark: any;
+    private currentTool: any;
     private currentFloor: Floor;
+    private currentPosition: any = { old: null, new: null };
 
-    constructor() {}
+    constructor() { }
 
     toolChanged(e) {
-        const tool = e.tool;
-        if (tool !== null && tool.name.length > 0) {
+        this.currentTool = e.tool;
+        if (this.currentTool !== null && this.currentTool.name.length > 0) {
             this.currentMark = L.icon({
-                iconUrl: `assets/maps/markers/${tool.name}.png`,
-                iconSize: tool.size,
+                iconUrl: `assets/maps/markers/${this.currentTool.name}.png`,
+                iconSize: this.currentTool.size,
             });
         } else {
             this.currentMark = null;
@@ -32,24 +38,23 @@ export class AreaMappingComponent {
 
     floorChanged(e) {
         this.mapLayer.clearLayers();
-        const floor: Floor = e.floor;
-        if (floor !== null) {
-            const bounds = new L.LatLngBounds(floor.bounds);
+        this.currentFloor = e.floor;
+        if (this.currentFloor !== null) {
+            const bounds = new L.LatLngBounds(this.currentFloor.bounds);
             this.imageMap.remove();
-            this.imageMap = L.imageOverlay(floor.imagePath, bounds);
+            this.imageMap = L.imageOverlay(this.currentFloor.imagePath, bounds);
             this.imageMap.addTo(this.map);
             this.map.fitBounds(bounds);
+            this.setMarkByList(this.currentFloor.coordinates);
         }
     }
 
     changedMap(map) {
         this.map = map;
-        this.map.on('click', (e) => {
-            if (this.currentMark) {
-                const marker = L.marker(e.latlng, {icon: this.currentMark, draggable: true, pane: 'markerPane'});
-                this.mapLayer.addLayer(marker);
-            }
-        });
+        this.map.on('click', (e) => { this.setMarkByEvent(e) });
+        setTimeout(() => {
+            this.barLevelStartIndex.next(0);
+        }, 0)
     }
 
     changedMapLayer(mapLayer) {
@@ -58,5 +63,39 @@ export class AreaMappingComponent {
 
     changedImageMap(imageMap) {
         this.imageMap = imageMap;
+    }
+
+    private setMarkByEvent(e) {
+        if (this.currentMark) {
+            const position = e.latlng;
+            this.currentFloor.coordinates.push(new Coordinate(position, this.currentTool.name));
+            this.createMarker(position, this.currentMark);
+        }
+    }
+
+    private setMarkByList(coordinates: Array<Coordinate>) {
+        coordinates.forEach((coordinate, index, array) => {
+            const mark = L.icon({
+                iconUrl: `assets/maps/markers/${coordinate.iconName}.png`,
+                iconSize: this.currentTool.size,
+            });
+            this.createMarker(coordinate.position, mark);
+        });
+    }
+
+    private createMarker(position: [number, number], mark: any) {
+        const marker = L.marker(position, { icon: mark, draggable: true, pane: 'markerPane' });
+        this.mapLayer.addLayer(marker);
+        marker.on('move', (event: any) => { 
+            this.currentPosition = { old: event.oldLatLng, new: event.latlng };
+        });
+        marker.on('moveend', () => { this.updateMark() });
+    }
+
+    private updateMark() {
+        const index = this.currentFloor.coordinates.findIndex(coordinate => coordinate.position === this.currentPosition.old);
+        if (index > -1) {
+            this.currentFloor.coordinates[index].position = this.currentPosition.new;
+        }
     }
 }
--- frontend/src/app/models/coordinate.model.ts
+++ frontend/src/app/models/coordinate.model.ts
@@ -1,21 +1,23 @@
 export class Coordinate {
     id: number;
-    lastPosition: number[];
-    private _position: number[];
+    iconName: string;
+    lastPosition: [number, number];
+    private _position: [number, number];
 
-    set position(newPosition: number[]) {
+    set position(newPosition: [number, number]) {
         if (this._position !== newPosition) {
             this.lastPosition = this._position;
         }
         this._position = newPosition;
     }
 
-    get position() : number[] {
+    get position() : [number, number] {
         return this._position;
     }
 
-    constructor(position: number[]) {
+    constructor(position: [number, number], iconName: string) {
         this.position = position;
+        this.iconName = iconName;
     }
 
 
