c00a5cfa2f5dfc83bef93e5947cbdc01e273da14
Bruno Azzi <bruno.azzi@icloud.com>
2017-06-07 03:22:56 -0300

    Add missing card files, and improves the mock, it adds the title, address and sponsor

--- frontend/src/app/mocks/construction-site/construction-site.ts
+++ frontend/src/app/mocks/construction-site/construction-site.ts
@@ -1,9 +1,23 @@
 export class ConstructionSite {
     status: string;
     image: string;
+    title: string;
+    address: string;
+    // TODO improve this later, it should be a worker object
+    sponsor: string;
 
-    constructor(status: string, image: string) {
+    constructor(
+        status: string,
+        image: string,
+        title: string,
+        address: string,
+        sponsor: string
+    ) {
         this.status = status;
         this.image = image;
+        this.title = title;
+        this.address = address;
+        this.sponsor = sponsor;
+
     }
 }
