76f7f372efa344fefcd2149ba67d4cec08e7f7dd
Rodolfo <rdlorbieski@gmail.com>
2017-06-08 09:14:53 -0300

    Updating emotional panel

--- frontend/src/app/views/painelEmocional/painelEmocional.component.ts
+++ frontend/src/app/views/painelEmocional/painelEmocional.component.ts
@@ -11,8 +11,6 @@ export class PainelEmocionalComponent {
 
     canvas: any;
     ctx: any;
-    // private width: number = 800;
-    // private height: number = 600;
 
     public showPopup() {
 
@@ -26,16 +24,9 @@ export class PainelEmocionalComponent {
     };
 
     onSelectChange = ($event: any): void => {
-
         this.canvas = document.getElementById('cnvs');
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext("2d");
-
-
-
-        console.log(this.canvas);
-        console.log('event => ', $event);
-        console.log('index => ', $event.index);
     }
 
 
