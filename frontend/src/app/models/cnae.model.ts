6c5913f4c2a0438d6bc2a791d73b6072bf483c47
Tiago Peixoto <tiago@plyom.com.br>
2017-07-06 11:36:15 -0300

    Adjusts after code review: do not drop database and remove some logging stuff

--- frontend/src/app/models/cnae.model.ts
+++ frontend/src/app/models/cnae.model.ts
@@ -3,13 +3,6 @@ export class Cnae {
     code: string;
     description: string;
 
-    // constructor()
-    // constructor(_id: number, _code: string, _description: string) {
-    //     this.id = _id;
-    //     this.code = _code;
-    //     this.description = _description;
-    // }
-
     initializeWithJson(json: any) {
         this.id = json.id;
         this.code = json.code;
