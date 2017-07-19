12a547af96cec386fc4bb8deb582da202c546a0d
Tiago Peixoto <tiago@plyom.com.br>
2017-07-19 19:55:52 -0300

    Changes backend url to use environment variables

--- frontend/src/app/services/http-client.service.ts
+++ frontend/src/app/services/http-client.service.ts
@@ -1,54 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class HttpClientService {
 
-  protected url = "http://localhost:8080";
-  private authToken
+  protected url = environment.backendUrl;
+  private authToken;
 
   constructor(private http: Http, private router: Router) {}
 
   standardHeaders() {
-    var headers = new Headers();
-    headers.append('Content-Type', 'application/json')
-    if (this.authToken) headers.append('X-Authorization', this.authToken)
-    return { headers: headers }
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    if (this.authToken) headers.append('X-Authorization', this.authToken);
+    return { headers: headers };
   }
 
   blankHeaders() {
-    var headers = new Headers();
+    const headers = new Headers();
     //headers.append('Content-Type', 'multipart/form-data')
-    this.authToken && headers.append('X-Authorization', this.authToken)
-    return { headers: headers }
+    this.authToken && headers.append('X-Authorization', this.authToken);
+    return { headers: headers };
   }
 
   setAuthToken(token : string) {
-    this.authToken = token
+    this.authToken = token;
   }
 
   post(path: String, params) {
     return this.http.post(this.url + path, params, this.standardHeaders())
       .map((response: Response) => {
-        return response.text().length > 0 ? response.json() : {}
+        return response.text().length > 0 ? response.json() : {};
       });
   }
 
   postWithNoHeaders(path: String, params) {
     return this.http.post(this.url + path, params, this.blankHeaders())
       .map((response: Response) => {
-        return response ? response : {}
+        return response ? response : {};
       });
   }
 
   get(path: string) {
     return this.http.get(this.url + path, this.standardHeaders())
       .map((response: Response) => {
-        return response.json()
-      })
+        return response.json();
+      });
   }
 
   getAbsolutePath(path: string) {
@@ -58,7 +58,7 @@ export class HttpClientService {
   put(path: String, params) {
     return this.http.put(this.url + path, params, this.standardHeaders())
       .map((response: Response) => {
-        return response.text().length > 0 ? response.json() : {}
+        return response.text().length > 0 ? response.json() : {};
       });
   }
 
